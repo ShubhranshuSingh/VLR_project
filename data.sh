c9035e43894201898c71a6bae9652080f5cea443
Ubuntu <ubuntu@ip-172-31-34-90.us-east-2.compute.internal>
2022-04-07 23:15:53 +0000

    Fixed COCO unzip, Added MDETR annotations

--- data.sh
+++ data.sh
@@ -1,12 +1,23 @@
+# Make dir
 mkdir data/
 mkdir data/images
 
+# Prepare COCO 2014 train data
 wget http://images.cocodataset.org/zips/train2014.zip -P data/images/
+unzip data/images/train2014.zip -d data/images/
 rm data/images/train2014.zip
 
-unzip data/images/train2014.zip -d data/images/
+# Annotations
+wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip -P data/
+unzip data/annotations_trainval2014.zip -d data/
+rm data/annotations_trainval2014.zip
 
+# RefCOCO original
 wget https://bvisionweb1.cs.unc.edu/licheng/referit/data/refcoco.zip -P data/
-
 unzip data/refcoco.zip -d data/
 rm data/refcoco.zip
+
+# MDETR cleaned annotations
+wget https://zenodo.org/record/4729015/files/mdetr_annotations.tar.gz -P data/
+tar -xf mdetr_annotations.tar.gz
+rm mdetr_annotations.tar.gz
